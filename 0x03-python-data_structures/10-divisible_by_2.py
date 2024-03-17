80cfd887a11106efcf3cb1919dc17907a663b463
Loisyy <nkeiruloisy@yahoo.com>
2024-03-16 23:03:25 -0700

    update 1

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    boolist = my_list[:]
-    for count, num in enumerate(my_list):
-        if num % 2 == 0:
-            boolist[count] = True
-        else:
-            boolist[count] = False
-    return(boolist)
+    new_list = []
+    if my_list:
+        for num in my_list:
+            new_list.append(False if num % 2 else True)
+        return new_list
