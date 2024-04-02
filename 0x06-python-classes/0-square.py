ed5c638d42a281d23723e18242d2f076f461d535
Loisyy <nkeiruloisy@yahoo.com>
2024-04-02 16:11:09 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """This represents a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """This represents a square."""
+
+    def __init__(self, size):
+        """This initializes a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
