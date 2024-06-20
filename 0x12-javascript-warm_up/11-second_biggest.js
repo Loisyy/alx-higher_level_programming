04fbb26186186cee6d13f26079e6d41fb87297fa
root <nkeiruloisy@yahoo.com>
2024-06-19 17:41:31 -0700

    update changes

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,13 +2,7 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const arr = process.argv.slice(2).map(Number).filter(n => !isNaN(n));
-
-  if (arr.length < 2) {
-    console.log('0');
-  } else {
-    const second = arr.sort((a, b) => b - a)[1];
-    console.log(second);
-  }
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
-
