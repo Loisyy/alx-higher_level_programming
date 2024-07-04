e20e0ab244c5bebca8fae1f272a3cee290914d2a
Nkeiru Lois <nkeiruloisy@yahoo.com>
2024-07-04 16:01:44 -0700

    0x10-python-network_0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,3 @@
-Updates to keyboard shortcuts … On Thursday 1 August 2024, Drive keyboard shortcuts will be updated to give you first-letter navigation.Learn more
 #!/usr/bin/python3
 """Finds a peak in a list of unsorted integers"""
 
